84f1bbcefc1e982cfee543047eb326efe830f207
Jorgen <jorgen@hookham>
2020-11-02 09:04:02 -0800

    add camera bounds

--- src/scenes/chapter-1/Tavern.ts
+++ src/scenes/chapter-1/Tavern.ts
@@ -1,15 +1,26 @@
 import Player from '../../sprites/Player';
 import playerAnimations from '../../animations/Player';
 import LevelTiles from '../../assets/tilesets/szadi-caves/level-tiles.png';
+import UtilTiles from '../../assets/tilesets/utils/util-colors.png';
 import Dwarf from '../../assets/dwarf.png';
 import Map from '../../assets/tilemaps/tavern.json';
+
+type LayerConf = [
+    string,
+    Phaser.Tilemaps.Tileset,
+    boolean,
+][];
 
 export default class TavernScene extends Phaser.Scene {
 
     private player: Player;
     private map: Phaser.Tilemaps.Tilemap;
-    private levelTiles: Phaser.Tilemaps.Tileset;
-    private groundLayer: Phaser.Tilemaps.StaticTilemapLayer;
+    private tileSets: {
+        [key: string]: Phaser.Tilemaps.Tileset;
+    } = {};
+    private layers: {
+        [key: string]: Phaser.Tilemaps.StaticTilemapLayer | Phaser.Tilemaps.DynamicTilemapLayer;
+    } = {};
 
     constructor (conf: Phaser.Types.Scenes.SettingsConfig) {
         super(conf);
@@ -25,23 +36,32 @@ export default class TavernScene extends Phaser.Scene {
         });
         this.load.tilemapTiledJSON('map', Map);
         this.load.image('level-tiles', LevelTiles);
+        this.load.image('util-tiles', UtilTiles);
     }
 
     create () {
         const height = this.sys.game.config.height as number;
         const width = this.sys.game.config.width as number;
-    
+
         // Map
+        const mapScale = 2.5;
+        const mapOriginX = width / 2 - width;
+        const mapOriginY = height / 2;
+
         this.map = this.make.tilemap({ key: 'map', tileWidth: 16, tileHeight: 16, });
-        this.levelTiles = this.map.addTilesetImage('level-tiles')
-        this.groundLayer = this.map.createStaticLayer(
-            'floor',
-            this.levelTiles,
-            width / 2 - width,
-            height / 2,
-        );
-        this.groundLayer.setScale(2.5);
-        this.map.setCollisionBetween(700, 1600)
+        this.tileSets.level = this.map.addTilesetImage('level-tiles');
+        this.tileSets.util = this.map.addTilesetImage('util-tiles');
+        
+        const layers: LayerConf = [
+            ['background', this.tileSets.level, true],
+            ['floor', this.tileSets.level, true],
+            ['camera-bounds', this.tileSets.level, false],
+        ];
+        layers.forEach(l => {
+            let n = this.map.createStaticLayer(l[0], l[1], mapOriginX, mapOriginY);
+            n.setScale(mapScale)
+            this.layers[l[0]] = n;
+        });
 
         // Player
         this.player = new Player(
@@ -50,10 +70,19 @@ export default class TavernScene extends Phaser.Scene {
             height + 560,
             'dwarf'
         );
-        this.physics.world.addCollider(this.player, this.groundLayer)
 
-        // Follow player
+        // Collision
+        this.map.setCollisionBetween(0, 1600, true, true, 'floor');
+        this.physics.world.addCollider(this.player, this.layers.floor)
+
+        // Camera
         this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBounds(
+            mapOriginX,
+            mapOriginY,
+            this.layers['camera-bounds'].displayWidth,
+            this.layers['camera-bounds'].displayHeight,
+        );
     }
 
     update () {
