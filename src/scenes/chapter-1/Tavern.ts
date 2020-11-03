ccd8d5441ba428d84479cd26c1ed7df75dd8ca3d
Jorgen <jorgen@hookham>
2020-11-03 02:34:36 -0800

    crtl-z to the rescue

--- src/scenes/chapter-1/Tavern.ts
+++ src/scenes/chapter-1/Tavern.ts
@@ -1,7 +1,7 @@
 import LevelTiles from 'assets/tilesets/szadi-caves/level-tiles.png';
 import UtilTiles from 'assets/tilesets/utils/util-colors.png';
 import Map from 'assets/tilemaps/tavern.json';
-import GameScene from 'scenes/GameScene';
+import GameScene, { LayerConf } from 'scenes/GameScene';
 
 export default class TavernScene extends GameScene {
 
@@ -10,15 +10,26 @@ export default class TavernScene extends GameScene {
         { key: 'level-tiles',   accessor: 'level',  asset: LevelTiles,},
         { key: 'util-tiles',    accessor: 'util',   asset: UtilTiles, },
     ];
-    protected mapTileLayersConf = [
-        {accessor: 'background', tileSet: this.tileSets.level, visible: true},
-        {accessor: 'floor', tileSet: this.tileSets.level, visible: true},
-        {accessor: 'foreground', tileSet: this.tileSets.level, visible: true},
-        {accessor: 'cameraBounds', tileSet: this.tileSets.level, visible: false},
-    ]
 
     preloadHook () {}
 
-    createHook () {}
+    createHook () {
+        // Map
+        this.map = this.make.tilemap({ key: 'map' });
+        this.tileSets.level = this.map.addTilesetImage('level-tiles');
+        this.tileSets.util = this.map.addTilesetImage('util-tiles');
+        
+        const layers: LayerConf = [
+            ['background', this.tileSets.level, true],
+            ['floor', this.tileSets.level, true],
+            ['foreground', this.tileSets.level, true],
+            ['cameraBounds', this.tileSets.level, false],
+        ];
+        layers.forEach(l => {
+            let n = this.map.createStaticLayer(l[0], l[1], 0, 0);
+            n.setScale(this.mapScale)
+            this.layers[l[0]] = n;
+        });
+    }
     
 }
